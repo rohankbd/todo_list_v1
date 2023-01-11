d96f10239a839f3cf53ec7db11a080ac3056891c
rohankbd <86970070+rohankbd@users.noreply.github.com>
2023-01-11 20:22:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food","Cook Food"];
+const workItems = [];
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+  res.render("list", {
+    listTitle: day, newListItems: items
+  });
+
+});
+
+app.post("/",function(req,res){
+  const item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
+
+app.post("/work",function(req,res){
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
